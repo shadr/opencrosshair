ca17db105465bbea11f8edaac9e47968c482014a
Nesterov Nikita <shadr.nn@gmail.com>
2026-02-26 13:10:44 +0300

    fix: remove unnecessary packages from devenv.nix

--- devenv.nix
+++ devenv.nix
@@ -1,39 +1,20 @@
 {
   pkgs,
   lib,
-  config,
-  inputs,
   ...
 }:
 
 {
   packages = with pkgs; [
-    glfw
-    cmake
-    clang
     wayland
     libxkbcommon
-    xorg.libX11
     pkg-config
-    libxrandr
-    xorg.libX11
-    xorg.libXrandr
-    xorg.libXinerama
-    xorg.libXcursor
-    xorg.libXi
     vulkan-loader
   ];
   env.LD_LIBRARY_PATH =
     with pkgs;
     lib.makeLibraryPath [
-      libGL
-      xorg.libX11
-      xorg.libXrandr
-      xorg.libXinerama
-      xorg.libXcursor
-      xorg.libXi
       libxkbcommon
-      libxrandr
       wayland
       vulkan-loader
     ];
