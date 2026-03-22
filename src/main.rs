cd817a6a6daad0343419edac1928de699ae59c26
Nesterov Nikita <shadr.nn@gmail.com>
2026-03-22 08:53:13 +0300

    feat: use the name of a display instead of an index

--- src/main.rs
+++ src/main.rs
@@ -6,7 +6,7 @@ use raw_window_handle::{
 use smithay_client_toolkit::{
     compositor::{CompositorHandler, CompositorState},
     delegate_compositor, delegate_layer, delegate_output, delegate_registry, delegate_shm,
-    output::{OutputHandler, OutputState},
+    output::{OutputHandler, OutputInfo, OutputState},
     reexports::protocols_wlr::foreign_toplevel::v1::client::{
         zwlr_foreign_toplevel_handle_v1::{self, ZwlrForeignToplevelHandleV1},
         zwlr_foreign_toplevel_manager_v1::ZwlrForeignToplevelManagerV1,
@@ -30,7 +30,10 @@ use wayland_backend::client::ObjectId;
 use wayland_client::{
     Connection, Dispatch, Proxy, QueueHandle,
     globals::registry_queue_init,
-    protocol::{wl_output, wl_region, wl_surface},
+    protocol::{
+        wl_output::{self, WlOutput},
+        wl_region, wl_surface,
+    },
 };
 use wgpu::util::DeviceExt;
 
@@ -109,9 +112,9 @@ struct Args {
     #[arg(short = 's', long = "scale", default_value_t = 0.8)]
     scale: f32,
 
-    /// Index of the display/output to show the crosshair on (e.g., 0, 1)
+    /// Name of the display/output to show the crosshair on (e.g., HDMI-A-1, DP-1)
     #[arg(short = 'd', long = "display")]
-    display: Option<usize>,
+    display: Option<String>,
 
     /// Path to the crosshair image file (default: embedded cross.png)
     #[arg(short = 'i', long = "image")]
@@ -155,6 +158,7 @@ fn main() {
 
     let conn = Connection::connect_to_env().unwrap();
     let (globals, mut event_queue) = registry_queue_init(&conn).unwrap();
+
     let qh = event_queue.handle();
 
     let layer_shell = LayerShell::bind(&globals, &qh).expect("layer shell is not available");
@@ -169,16 +173,18 @@ fn main() {
 
     let mut output = None;
 
-    let output_state = OutputState::new(&globals, &qh);
-    // TODO: Currently we can't get WlOutput info here, according to smithay docs compositor didn't send output info at this moment yet
-    let outputs = output_state
-        .outputs()
-        // .map(|wloutput| output_state.info(&wloutput))
-        // .flatten()
-        .collect::<Vec<_>>();
-    if let Some(display_index) = args.display {
-        if let Some(selected_output) = outputs.get(display_index) {
-            output = Some(selected_output)
+    let outputs = get_outputs_with_info(&conn);
+    dbg!(&outputs);
+    if let Some(selected_output_name) = args.display {
+        for (wloutput, outputinfo) in &outputs {
+            if let Some(outputinfo) = outputinfo {
+                if let Some(output_name) = &outputinfo.name {
+                    if *output_name == selected_output_name {
+                        output = Some(wloutput);
+                        break;
+                    }
+                }
+            }
         }
     }
 
@@ -972,5 +978,113 @@ delegate_registry!(OpenCrosshair);
 impl ShmHandler for OpenCrosshair {
     fn shm_state(&mut self) -> &mut Shm {
         &mut self.shm
+    }
+}
+
+fn get_outputs_with_info(conn: &Connection) -> Vec<(WlOutput, Option<OutputInfo>)> {
+    // Now create an event queue and a handle to the queue so we can create objects.
+    let (globals, mut event_queue) = registry_queue_init(conn).unwrap();
+    let qh = event_queue.handle();
+
+    // Initialize the registry handling so other parts of Smithay's client toolkit may bind
+    // globals.
+    let registry_state = RegistryState::new(&globals);
+
+    // Initialize the delegate we will use for outputs.
+    let output_delegate = OutputState::new(&globals, &qh);
+
+    // Set up application state.
+    //
+    // This is where you will store your delegates and any data you wish to access/mutate while the
+    // application is running.
+    let mut list_outputs = ListOutputs {
+        registry_state,
+        output_state: output_delegate,
+    };
+
+    // `OutputState::new()` binds the output globals found in `registry_queue_init()`.
+    //
+    // After the globals are bound, we need to dispatch again so that events may be sent to the newly
+    // created objects.
+    event_queue.roundtrip(&mut list_outputs).unwrap();
+
+    // Now our outputs have been initialized with data, we may access what outputs exist and information about
+    // said outputs using the output delegate.
+    let mut outputs = Vec::new();
+    for output in list_outputs.output_state.outputs() {
+        let info = list_outputs.output_state.info(&output);
+        outputs.push((output, info));
+    }
+    outputs
+}
+
+// Copy pasted from smithay-client-toolkit examples/list_outputs.rs
+struct ListOutputs {
+    registry_state: RegistryState,
+    output_state: OutputState,
+}
+
+// In order to use OutputDelegate, we must implement this trait to indicate when something has happened to an
+// output and to provide an instance of the output state to the delegate when dispatching events.
+impl OutputHandler for ListOutputs {
+    // First we need to provide a way to access the delegate.
+    //
+    // This is needed because delegate implementations for handling events use the application data type in
+    // their function signatures. This allows the implementation to access an instance of the type.
+    fn output_state(&mut self) -> &mut OutputState {
+        &mut self.output_state
+    }
+
+    // Then there exist these functions that indicate the lifecycle of an output.
+    // These will be called as appropriate by the delegate implementation.
+
+    fn new_output(
+        &mut self,
+        _conn: &Connection,
+        _qh: &QueueHandle<Self>,
+        _output: wl_output::WlOutput,
+    ) {
+    }
+
+    fn update_output(
+        &mut self,
+        _conn: &Connection,
+        _qh: &QueueHandle<Self>,
+        _output: wl_output::WlOutput,
+    ) {
+    }
+
+    fn output_destroyed(
+        &mut self,
+        _conn: &Connection,
+        _qh: &QueueHandle<Self>,
+        _output: wl_output::WlOutput,
+    ) {
+    }
+}
+
+// Now we need to say we are delegating the responsibility of output related events for our application data
+// type to the requisite delegate.
+delegate_output!(ListOutputs);
+
+// In order for our delegate to know of the existence of globals, we need to implement registry
+// handling for the program. This trait will forward events to the RegistryHandler trait
+// implementations.
+delegate_registry!(ListOutputs);
+
+// In order for delegate_registry to work, our application data type needs to provide a way for the
+// implementation to access the registry state.
+//
+// We also need to indicate which delegates will get told about globals being created. We specify
+// the types of the delegates inside the array.
+impl ProvidesRegistryState for ListOutputs {
+    fn registry(&mut self) -> &mut RegistryState {
+        &mut self.registry_state
+    }
+
+    registry_handlers! {
+        // Here we specify that OutputState needs to receive events regarding the creation and destruction of
+        // globals.
+        OutputState,
     }
 }
